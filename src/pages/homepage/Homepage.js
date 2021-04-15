116487e1ce11ed9ae326f4628e5e2a5ce0614e0e
JigmeT12 <jigmetashi02@gmail.com>
2021-04-15 23:21:29 +0600

    small fixes

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,6 +1,6 @@
 import styles from './homepage.module.css';
-
-
+import {Button} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 const Homepage = () => {
     return (
         <div className={styles.homepageContainer}>
@@ -10,6 +10,9 @@ const Homepage = () => {
                     <span className={styles.j}>J</span>igme <span className={styles.t}>T</span>ashi <br /> <span className={styles.n}>N</span>amgyal
                 </p>
                 <p className={styles.lilInfo}>Nincompoop Since 2001</p>
+            </div>
+            <div className={styles.btnContainer}>
+                <Button className={styles.btnAbout} variant="outline-warning"><Link to="/aboutMe">Explore</Link></Button>
             </div>
         </div>
     )
