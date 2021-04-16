76f24a302310aef5c499ec410bc7deff03d2cc46
JigmeT12 <jigmetashi02@gmail.com>
2021-04-16 18:21:05 +0600

    small changes

--- src/pages/aboutMe/AboutMe.js
+++ src/pages/aboutMe/AboutMe.js
@@ -7,7 +7,6 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import {red} from '@material-ui/core/colors';
