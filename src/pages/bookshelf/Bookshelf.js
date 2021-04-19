7ebd3a82df6e794997c8a191cd50a3c32fdcd0e8
JigmeT12 <jigmetashi02@gmail.com>
2021-04-19 21:56:38 +0600

    ADDED: pagination icons and books

--- src/pages/bookshelf/Bookshelf.js
+++ src/pages/bookshelf/Bookshelf.js
@@ -2,11 +2,12 @@ import styles from './book.module.css';
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
-
+import {bookshelfData} from './bookshelfData';
 const useStyles = makeStyles((theme) => ({
     root: {
         '& > *': {
-            marginTop: theme.spacing(2)
+            marginTop: theme.spacing(2),
+            marginBottom: "40px"
         }
     }
 }));
@@ -20,12 +21,23 @@ const Bookshelf = () => {
             </div>
             <div className={styles.bookshelfMainContent}>
                 <div className={styles.booksList}>
-                    
+                    {bookshelfData.map((data,key) => (
+                        <div key={key} className={styles.bookListMainContent}>
+                            <div className={styles.imgCoverHolder}>
+                                <img className={styles.imgCover} src={data.imgName} alt={data.bookName} />
+                            </div>
+                            <div className={styles.infoBookHolder}>
+                                <p className={styles.infoBook}>{data.lilAboutBook}</p>
+                            </div>
+                        </div>
+                    ))}
                     <div className={classes.root}>
                         <Pagination shape="rounded" showFirstButton showLastButton count={6} color="secondary"/>
                     </div>
                 </div>
-                <div className={styles.popularPost}></div>
+                <div className={styles.popularPost}>
+                    {/* <p>Popular Post</p> */}
+                </div>
             </div>
         </div>
     )
