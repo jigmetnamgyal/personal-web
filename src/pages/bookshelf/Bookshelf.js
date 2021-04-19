92436c1cdd147d85e1a5302c137c1c25e8926406
JigmeT12 <jigmetashi02@gmail.com>
2021-04-19 20:27:22 +0600

    ADDED: pagination

--- src/pages/bookshelf/Bookshelf.js
+++ src/pages/bookshelf/Bookshelf.js
@@ -1,15 +1,32 @@
 import styles from './book.module.css';
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            marginTop: theme.spacing(2)
+        }
+    }
+}));
 
 const Bookshelf = () => {
+    const classes = useStyles();
     return (
         <div className={styles.bookShelf}>
-             <div className={styles.headerAbout}>
+            <div className={styles.headerAbout}>
                 <p className={styles.contentHeader}>Bookshelf</p>
-             </div>
-             <div className={styles.bookshelfMainContent}>
-                 <div className={styles.booksList}></div>
-                 <div className={styles.popularPost}></div>
-             </div>
+            </div>
+            <div className={styles.bookshelfMainContent}>
+                <div className={styles.booksList}>
+                    
+                    <div className={classes.root}>
+                        <Pagination shape="rounded" showFirstButton showLastButton count={6} color="secondary"/>
+                    </div>
+                </div>
+                <div className={styles.popularPost}></div>
+            </div>
         </div>
     )
 }
