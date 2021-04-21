b9fd696f8c93af9a8f079163b8ecbd0e2ff2f9c5
JigmeT12 <jigmetashi02@gmail.com>
2021-04-21 13:37:57 +0600

    ADDED: projects starter file

---
+++ src/pages/projects/Projects.js
@@ -0,0 +1,11 @@
+import styles from './project.module.css';
+
+const Projects = () => {
+    return (
+        <div className={styles.projectsMainContainer}>
+            
+        </div>
+    )
+}
+
+export default Projects
