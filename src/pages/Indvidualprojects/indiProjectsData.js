3a634d65fe5d1ef89f262711fefac4f4f9f5397c
JigmeT12 <jigmetashi02@gmail.com>
2021-04-23 00:18:56 +0600

    ADDED: parrallex

--- src/pages/Indvidualprojects/indiProjectsData.js
+++ src/pages/Indvidualprojects/indiProjectsData.js
@@ -11,8 +11,12 @@ import six from '../../assets/arexp/6.jpeg';
 export const indiProjectsData = [
     {
         "id":0,
-        "projectName": "ArExp",
-        "images": [arexp, frontImg, heartImg, one, two, three, four, five, six]
+        "firstName": ["A", "r"],
+        "lastName": ["E", "x", "p"],
+        "images": [arexp, frontImg, heartImg, one, two, three, four, five, six],
+        "type": "Start-up Idea",
+        "founder": "Jigme Tashi Namgyal",
+        "projectsInfo": "AR Exp is a start-up idea powered by the frontier technology of AR and blockchain technology. A fun environment for student to learn by providing augmented reality experience and Allow Kids to Learn, Earn, Create, Sell and Buy their Creative Achievements. The student will be required to scan a custom-made books and card where they can get access to 3-d labelled model of the study material. We are going to bring life to the inanimate visuals in the text books. It is also a motivation booster for the student. We are going to Bring in gamification and rewarding experience. Students can earn digital token called DrukCoin as and when they complete homework’s, tests, courses etc. DrukCoin can be used to buy digital assets such as digital arts, badges, themes etc from the store and can also be sold to other people. (such as in NFTs) Ar Exp will also allow the teachers and parents to keep track of the students’ performance in school and visualize with live data on the website on daily basis. Ar Exp will also solve the traditional means of storing and validating students’ records and certificate by digitally storing it in a block chain technology. ArExp will enhance the interest and interaction digitally. The AR Exp application will be accessible to instructor for online or physical activities and for students who can access the application and learn by themselves."
     },
     {
         name: "lol"
